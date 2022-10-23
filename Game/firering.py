1b904fbecaabb8517bc658acf64041b86870308f
ysj4415@home <68343822+ysj4415@users.noreply.github.com>
2022-10-23 16:09:55 +0900

    firering 변경
    상속을 이용한 단계 조절 가능

--- Game/firering.py
+++ Game/firering.py
@@ -1,40 +1,35 @@
 from pico2d import *
+from Framework.Pawn import *
 
 damagebox = []
 
 ground = 50
-frame = None
 
-class FireRing:
+class FireRing(pawn):
     image = None
     def __init__(self, x, y, step):
-        self.x = x
-        self.y = y
+        super().__init__(x,y + (step * 25))
+        self.image_info = [0, 0, 60, 100]
 
-        self.frame = 0
-        global frame
-        frame = 0
+        self.frame_number = 2
+        self.frame_speed = 50
 
         if step >= 3 or step < 0:
             print("error: step over 3")
         else: self.step = step
 
-        if self.image == None: self.image = load_image('res/character/monster_firering.png')
+        self.loadimage('res/character/monster_firering.png')
         self.makeDamageBox()
     def makeDamageBox(self):
-        damagebox.append(((self.x - 10, 0), (self.x + 10, 25 * (self.step + 1))))
-    def update(self):
-        global frame
-        self.frame = (frame // 50) % 2
-        frame += 1
+        x = self.position.translate.x
+        y = self.position.translate.y
+        damagebox.append(((x - 10, 0), (x + 10, y - 30)))
+
     def draw(self):
         for i in range(self.step):
             self.image.clip_composite_draw(2 * 60, 0 * 100,
                                            60, 100, 0, '',
-                                           self.x,
-                                           self.y + 25 * i, 60, 100)
-        self.image.clip_composite_draw(self.frame*60, 0 * 100,
-                                        60, 100, 0, '',
-                                       self.x,
-                                       self.y + 25 * self.step, 60, 100)
+                                           self.position.translate.x, self.position.translate.y - 25 * (i + 1),
+                                           60, 100)
+        super().draw()
 
